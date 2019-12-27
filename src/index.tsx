1540f42ee70589e221b8d33076cd938fbff3a01b
King Dev <kd19850511@gmail.com>
2019-12-27 04:27:20 -0500

    show uploader component

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { GlobalStyle } from './global-style'
+import Uploader from './components/logo-uploader'
 
 console.info(`⚛️ ${React.version}`)
 
 const App = () => (
   <>
     <GlobalStyle />
-    Hello World
+    <Uploader />
   </>
 )
 
